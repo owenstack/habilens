97915d22c29b4dec1eadc5d996d611d6785b5760
owenstack <jenukohaefobi@gmail.com>
2025-02-09 19:27:52 +0100

    Add tenant, landlord, and marketing pages with layouts; update Tailwind CSS configuration and global styles

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export default function MarketingLayout({children}: {children: ReactNode}) {
+    return <>{children}</>
+}
---
+++ app/tenant/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default async function TenantLayout({children}: {children: ReactNode}) {
+    return <>{children}</>
+}
